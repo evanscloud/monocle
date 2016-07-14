ab481df79940d7adc64d8b64e4c330bd827f8b72
evanscloud <dijkstrascloud@gmail.com>
2016-07-14 00:37:14 -0400

    Small revisions to design of application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" }
+
+  resources :books, only: [:index, :create, :show]
+
+  resources :collections do
+    resources :books, only: [:show, :update, :destroy]
+    resources :comments
+  end
+  resources :users do
+    resources :collections
+  end
+
   root to: 'welcome#index'
 
 end
