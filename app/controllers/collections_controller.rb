bcdd6a73be721ce01d7ae9d4b50dfa51bfc81492
evanscloud <dijkstrascloud@gmail.com>
2016-07-15 15:14:44 -0400

    Build out collections controller and its views

---
+++ app/controllers/collections_controller.rb
@@ -0,0 +1,60 @@
+class CollectionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_collection, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @collections = Collection.where(user_id: current_user.id)
+  end
+
+  def new
+    if current_user.username = params[:user_id]
+      @collection = current_user.collections.build
+    else
+      redirect_to new_user_collection_path(current_user), alert: "Sorry, you can only create a collection for yourself."
+    end
+  end
+
+  def create
+    @collection = current_user.collections.create(collection_params)
+    if @collection.save
+      redirect_to user_collection_path(@collection.user_id, @collection.id), alert: "The collection has been added to your bookshelf. High fives all around!"
+    else
+      render :new, alert: "Sorry, there seems to be an error. Please try again."
+    end
+  end
+
+  def show
+    @user = current_user
+    @books = @collection.books
+    if @collection.nil?
+      redirect_to user_collections_path(@collection), alert: "Sorry, collection not found."
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @collection.update(collection_params)
+      redirect_to user_collection_path(@collection.user_id, @collection.id), alert: "Looks like it updated successfully. Woohoo!"
+    else
+      render :edit, alert: "Looks like something went wrong. Please try again."
+    end
+  end
+
+  def destroy
+    @collection.destroy
+    redirect_to user_collections_path
+  end
+
+  private
+  def set_collection
+    @collection = Collection.find(params[:id])
+  end
+
+  def collection_params
+    params.require(:collection).permit(:name, :user_id)
+  end
+end
+
+# , comment_ids: [], comments_attributes: [:content]
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -2,6 +2,12 @@ class Collection < ActiveRecord::Base
   belongs_to :user
   has_many :book_collections
   has_many :books, :through => :book_collections
-  has_many :comments
-  validates_uniqueness_of :name
+  validates :name, :presence => true
+
+  # def comments_attributes=(comment_attributes)
+  #   comment_attributes.values.each do |com_attr|
+  #     comment = Comment.find_or_create_by(content: com_attr[:content])
+  #     self.comments.build(content: comment[:content])
+  #   end
+  # end
 end
