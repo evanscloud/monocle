c9ca11515f64b85f607703f894ebd29c903659b1
evanscloud <dijkstrascloud@gmail.com>
2016-07-19 09:05:22 -0400

    Refactor index route in collections controller

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,21 +1,30 @@
 class CollectionsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index]
   before_action :set_collection, only: [:show, :edit, :update, :destroy]
 
   def index
-    @collections = Collection.where(user_id: current_user.id)
+    if params[:user_id]
+      @user = current_user
+      if @user.nil?
+        redirect_to root_path, alert: "ALERT! User not found."
+      else
+        @collections = @user.collections
+      end
+    else
+      @collections = Collection.all
+    end
   end
 
   def new
     if current_user.username = params[:user_id]
-      @collection = current_user.collections.build
+      @collection = Collection.new
     else
       redirect_to new_user_collection_path(current_user), alert: "Sorry, you can only create a collection for yourself."
     end
   end
 
   def create
-    @collection = current_user.collections.create(collection_params)
+    @collection = current_user.collections.build(collection_params)
     if @collection.save
       redirect_to user_collection_path(@collection.user_id, @collection.id), alert: "The collection has been added to your bookshelf. High fives all around!"
     else
