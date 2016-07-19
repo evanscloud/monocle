418f1f361d30a7d50a163d97df07961df377af10
evanscloud <dijkstrascloud@gmail.com>
2016-07-19 09:08:32 -0400

    Add search method to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,10 @@ class Book < ActiveRecord::Base
   has_many :book_collections
   has_many :collections, :through => :book_collections
   validates_presence_of :title, :author
+
+  def search(title)
+    GoogleBooks.search("#{title}").first
+  end
+
+
 end
