9d338a960fb61d60f5851e849662ee6f34063a45
evanscloud <dijkstrascloud@gmail.com>
2016-07-25 13:26:18 -0400

    Update nested resources route with shallow nesting

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" }
 
-  resources :users, only: [:show] do
+  resources :users, only: [:show], shallow: true do
     resources :collections do
       resources :books
     end
   end
-
-  resources :collections, only: [:index]
 
   root to: 'welcome#index'
 
