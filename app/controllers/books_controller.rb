672d367ef93681afe85300beaed8337790900281
evanscloud <dijkstrascloud@gmail.com>
2016-07-29 14:26:35 -0400

    Update routes for book controller; still incomplete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,38 +1,82 @@
 class BooksController < ApplicationController
-  before_action :set_collection
+  # before_action :set_collection
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
 
-  def new
-    if current_user.username = params[:user_id]
-      @book = Book.new
-    else
-      redirect_to user_collection_path(current_user, current_user.collection), alert: "Sorry, you can only add to your collection."
-    end
+  def index
   end
 
-  def create
-    # binding.pry
-    @book = Book.new
-    book = GoogleBooks.search(params[:book][:title]).first
+  def search
+    book = Book.search(params[:book][:title]).first
     if !book.nil?
       @book.info(book)
-      @book = @collection.books.build(@book)
+      book_params = @book.attributes
+      @book = @collection.books.build(book_params)
       if @book.save
-        redirect_to user_collection_path(@collection.user_id, @collection.id), alert: "The book has been added to your collection. Awesome sauce!"
+        redirect_to book_path(@book.id), alert: "The book has been added to your collection. Awesome sauce!"
       else
         render :new, alert: "Sorry, we couldn't find that. Please try a different search."
       end
     else
-      redirect_to new_user_collection_book_path, alert: "Sorry, we couldn't find that. Please try a different search."
+      redirect_to new_collection_book_path(@collection.id), alert: "Sorry, we couldn't find that. Please try a different search."
     end
+  end
+
+  def new
+    if @collection.id = params[:collection_id]
+      @book = Book.new
+    else
+      redirect_to collection_path(@collection.id), alert: "Sorry, you can only add to your own collection."
+    end
+  end
+
+  def create
+    @book = @collection.books.build(book_params)
+    binding.pry
+    @book.collection = @collection
+    @book.collections << @collection
+    if @book.save
+      redirect_to book_path(@book.id), alert: "The book has been added to your collection. Awesome sauce!"
+    else
+      render :new, alert: "Sorry, there seems to be an error. Please try again."
+    end
+  end
+
+  def show
+    @user = current_user
+    if @book.nil?
+      redirect_to user_collections_path(current_user), alert: "Sorry, couldn't find that book."
+    end
+    # @book = @collection.books.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), alert: "Your book has returned from it's glorious journey across the galaxy."
+    else
+      render :edit, alert: "Looks like something went wrong. Please try again."
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to user_collections_path(current_user.id), alert: "* mistakenly drops book over paper shredder *"
   end
 
   private
 
     def set_collection
-      @collection = Collection.find(params[:collection_id])
+      @book = Book.find(params[:id])
+      @collection = Collection.find(@book.collection_ids.first)
+    end
+
+    def set_book
+      @book = Book.find(params[:id])
     end
 
     def book_params
-      params.require(:book).permit(:title, :author, :publisher, :publisher_date, :categories, :average_rating, :ratings_count, :description, :price, :isbn, :buy_link, :image_link)
+      params.require(:book).permit(:title, :author, :publisher, :publisher_date, :categories, :average_rating, :ratings_count, :description, :price, :isbn, :buy_link, :image_link, :collection_id)
     end
 end
