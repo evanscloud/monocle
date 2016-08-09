6347469c803ecd450c4ebe332b6126a82f5d58d7
evanscloud <dijkstrascloud@gmail.com>
2016-08-08 23:24:49 -0400

    Turn off search feature

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,18 +4,22 @@ class Book < ActiveRecord::Base
   has_many :collections, :through => :book_collections
   validates_presence_of :title, :author
 
-  include GoogleBooks
-
-  def info(book)
-    self.title = book.title
-    self.author = book.authors
-    self.publisher = book.publisher
-    self.published_date = book.published_date
-    self.categories = book.categories
-    self.description = book.description
-    self.price = book.sale_info['retailPrice']['amount']
-    self.isbn = book.isbn
-    self.buy_link = book.sale_info['buyLink']
-    self.image_link = book.image_link
-  end
+  # include GoogleBooks
+  #
+  # def self.search(title)
+  #   GoogleBooks.search("#{title}")
+  # end
+  #
+  # def info(book)
+  #   self.title = book.title
+  #   self.author = book.authors
+  #   self.publisher = book.publisher
+  #   self.published_date = book.published_date
+  #   self.categories = book.categories
+  #   self.description = book.description
+  #   self.price = book.sale_info['retailPrice']['amount']
+  #   self.isbn = book.isbn
+  #   self.buy_link = book.sale_info['buyLink']
+  #   self.image_link = book.image_link
+  # end
 end
