a28caff30612d3bbc1215ea0de9d6e47dbda026d
evanscloud <dijkstrascloud@gmail.com>
2016-08-10 10:13:06 -0400

    Add comment resources to application

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :comments
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks", :registrations => "registrations" }
 
   resources :users, only: [:show], shallow: true do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713145112) do
+ActiveRecord::Schema.define(version: 20160810140613) do
 
   create_table "book_collections", force: :cascade do |t|
     t.integer  "book_id"
@@ -46,6 +46,14 @@ ActiveRecord::Schema.define(version: 20160713145112) do
   end
 
   add_index "collections", ["user_id"], name: "index_collections_on_user_id"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "collection_id"
+    t.string   "content"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
