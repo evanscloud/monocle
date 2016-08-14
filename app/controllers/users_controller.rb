6416e2ded08c3363b5aa67c1689753416bc7d755
evanscloud <dijkstrascloud@gmail.com>
2016-08-14 12:50:10 -0400

    Create account show page and list option on drop menu to see account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+    if current_user.id == params[:id].to_i
+      @user = current_user
+    else
+      redirect_to root_path, alert: "ACCESS DENIED."
+    end
+  end
+
 end
