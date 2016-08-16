87e82c687fd538ac7a01b5e6fcdd3d6faa802490
evanscloud <dijkstrascloud@gmail.com>
2016-08-15 23:17:46 -0400

    Update omniauth callback to persist account name as username

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,6 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
+      @user.update(username: request.env["omniauth.auth"].info.name)
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
@@ -13,6 +14,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
+      @user.update(username: request.env["omniauth.auth"].info.name)
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
     else
