58895a6ce64e4c91e5a997c5d2ba7d25be62c399
evanscloud <dijkstrascloud@gmail.com>
2016-08-25 14:29:40 -0400

    Refactor class methods with scope for collection model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -5,22 +5,17 @@ class Collection < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
   validates :name, :presence => true, :uniqueness => true
 
+  scope :most_recent, -> { order(created_at: :desc).limit(1) }
+  scope :most_books, -> { select('collections.*, count(books.id) as book_count').
+    joins(:books).
+    group('collections.id').
+    order('book_count DESC LIMIT 1') }
+
   def books_attributes=(books_attributes)
     books_attributes.values.each do |book_attr|
       book = Book.find_or_create_by(title: book_attr[:title], author: book_attr[:author], publisher: book_attr[:publisher], published_date: book_attr[:published_date], categories: book_attr[:categories], description: book_attr[:description], price: book_attr[:price], isbn: book_attr[:isbn], buy_link: book_attr[:buy_link], image_link: book_attr[:image_link])
 
       self.book_collections.build(book_id: book.id)
     end
-  end
-
-  def self.most_recent
-    self.order(created_at: :desc).limit(1)
-  end
-
-  def self.most_books
-    Collection.select('collections.*, count(books.id) as book_count').
-      joins(:books).
-      group('collections.id').
-      order('book_count DESC LIMIT 1')
   end
 end
