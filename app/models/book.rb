760ff86ed76e5f57a757f4f29d87c9340dc45931
evanscloud <dijkstrascloud@gmail.com>
2016-08-26 23:54:52 -0400

    Create join table has many relationship between book and genre

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,25 @@
 class Book < ActiveRecord::Base
   has_many :book_collections, :dependent => :destroy
   has_many :collections, :through => :book_collections
+  has_many :book_genres
+  has_many :genres, :through => :book_genres
   validates :title, :presence => true
   validates :author, :presence => true
+
+  def genres_attributes=(genres_attributes)
+    genres_attributes.each do |genre_attr|
+      unless genre_attr["name"].blank?
+        genre_attr.values.each do |attr|
+          genre = Genre.find_or_create_by(name: attr)
+
+          self.book_genres.build(genre_id: genre.id)
+        end
+      end
+    end
+  end
+
+  def display_genre
+    genre = self.genres.map { |genre| genre.name }
+    genre.join("")
+  end
 end
