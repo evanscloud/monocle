2524019d13cc4c013ef0d6a71ddc005f7460461c
evanscloud <dijkstrascloud@gmail.com>
2016-08-26 23:56:20 -0400

    Add seed data from users and list of genres

---
+++ db/seeds.rb
@@ -0,0 +1,56 @@
+# Users
+
+User.create(
+  username: "evan",
+  email: "evan@email.com",
+  password: "000000",
+  password_confirmation: "000000"
+)
+
+User.create(
+  username: "mphelps",
+  email: "mphelps@email.com",
+  password: "qqqqqq",
+  password_confirmation: "qqqqqq"
+)
+
+User.create(
+  username: "ubolt",
+  email: "ubolt@email.com",
+  password: "qqqqqq",
+  password_confirmation: "qqqqqq"
+)
+
+User.create(
+  username: "sbiles",
+  email: "sbiles@email.com",
+  password: "qqqqqq",
+  password_confirmation: "qqqqqq"
+)
+
+User.create(
+  username: "ndjoker",
+  email: "ndjoker@email.com",
+  password: "qqqqqq",
+  password_confirmation: "qqqqqq"
+)
+
+# Genres
+
+Genre.create(name: "comedy")
+Genre.create(name: "drama")
+Genre.create(name: "horror")
+Genre.create(name: "non-fiction")
+Genre.create(name: "realistic fiction")
+Genre.create(name: "romance")
+Genre.create(name: "satire")
+Genre.create(name: "tragedy")
+Genre.create(name: "tragicomedy")
+Genre.create(name: "classic")
+Genre.create(name: "fairytale")
+Genre.create(name: "mystery")
+Genre.create(name: "science fiction")
+Genre.create(name: "memoir")
+Genre.create(name: "biography/autobiography")
+Genre.create(name: "self-help")
+Genre.create(name: "reference")
