5b3be66f42cf7bd36face6c5f6a65e62df511277
evanscloud <dijkstrascloud@gmail.com>
2016-09-20 13:53:09 -0400

    Render user json data in collection

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -1,4 +1,5 @@
 class CollectionSerializer < ActiveModel::Serializer
   attributes :id, :name
   has_many :books, serializer: BookCollectionSerializer
+  has_one :user, serializer: UserCollectionSerializer
 end
---
+++ app/serializers/user_collection_serializer.rb
@@ -0,0 +1,3 @@
+class UserCollectionSerializer < ActiveModel::Serializer
+  attributes :id, :username
+end
