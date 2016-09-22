b0b246d06c33f4d192d888c1e35f7eedccb508ed
evanscloud <dijkstrascloud@gmail.com>
2016-09-21 23:14:53 -0400

    Render partial to display json data on index page

--- app/assets/javascripts/collections/all_collections.js
+++ app/assets/javascripts/collections/all_collections.js
@@ -1,28 +1,25 @@
 $(function() {
   $.getJSON('/collections.json', function(data) {
     var collections = data;
-    var i = 0;
+    var collectionText = '';
 
-    collectionText = '<table align="center">';
+    collectionText += '<table align="center">';
     collectionText += '<tr>';
     collectionText += '<th><u>NAME</u></th>';
     collectionText += '<th><u>COUNT</u></th>';
     collectionText += '</tr>';
     if (collections) {
-      while (i < collections.length) {
+      for (i = 0; i < collections.length; i++) {
         collectionText += '<tr>';
         collectionText += '<td><a href="collections/' + collections[i].id + '">' + collections[i].name + ' by ' + collections[i].user.username + '</a>';
         collectionText += '<td>' + collections[i].books.length + ' book(s)</td>';
         collectionText += '</tr>';
-        i++;
       };
-      $('.custlist').append(collectionText);
+      $('.all-collections').append(collectionText);
     }
     else {
-      collectionText = '<h5>Where did everyone go...?</h5>';
-      $('.custlist').append(collectionText);
+      collectionText += '<h5>Where did everyone go...?</h5>';
     };
     collectionText += '</table>';
-    $('.custlist').append(collectionText);
   });
 });
