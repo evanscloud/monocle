c0ba5610a8348c9b9dc9ce13cb0dfe57e8d72f61
evanscloud <dijkstrascloud@gmail.com>
2016-09-21 23:13:37 -0400

    Reorder json data output for collection

--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -1,6 +1,6 @@
 class CollectionSerializer < ActiveModel::Serializer
   attributes :id, :name
+  has_one :user, serializer: UserCollectionSerializer
   has_many :books, serializer: BookCollectionSerializer
   has_many :comments, serializer: CommentCollectionSerializer
-  has_one :user, serializer: UserCollectionSerializer
 end
