f1e0b35018599ee7781d3898c9308ee3b8785586
evanscloud <dijkstrascloud@gmail.com>
2016-09-27 16:29:52 -0400

    Render collection show page book json data

---
+++ app/assets/javascripts/collections/collection_show.js
@@ -0,0 +1,17 @@
+$(function(){
+  $.getJSON('/collections/' + collectionId + '.json', function(data){
+    var collection = new Collection(data.id, data.name, data.user, data.books, data.comments);
+    var collectionText = '';
+    
+    if (collection){
+      for (i = 0; i < collection.books.length; i++){
+        collectionText += '<li><a href="/books/"' + collection.books[i].id + '>' + collection.books[i].title + ' by ' + collection.books[i].author + '</li>';
+      };
+      $('#collection-books-show').append(collectionText);
+    }
+    else {
+      collectionText += '<h5>It\'s kinda lonely here...</h5>';
+      $('#collection-books-show').append(collectionText);
+    };
+  });
+});
