3dab461807bdbeff246e32714ad34c4aab322e61
evanscloud <dijkstrascloud@gmail.com>
2016-09-28 15:24:55 -0400

    Add book genre serializer to access genre json data

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,4 +1,5 @@
 class BookSerializer < ActiveModel::Serializer
 attributes :id, :title, :author, :publisher, :published_date, :description, :price, :isbn, :buy_link, :image_link
   has_many :collections
+  has_many :genres, serializer: BookGenreSerializer
 end
