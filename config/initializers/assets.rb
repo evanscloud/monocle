813c710d89de8ea3a05713151dfc834112e63829
evanscloud <dijkstrascloud@gmail.com>
2016-09-28 15:27:59 -0400

    Update header and asset pipeline to use book javascript for show page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,5 +16,8 @@ Rails.application.config.assets.precompile += %w( collections/collection_show.js
 Rails.application.config.assets.precompile += %w( collections/collection_model.js )
 Rails.application.config.assets.precompile += %w( collections/book_fields.js )
 
+Rails.application.config.assets.precompile += %w( books/book_show.js )
+Rails.application.config.assets.precompile += %w( books/book_model.js )
+
 Rails.application.config.assets.precompile += %w( users/user_show.js )
 Rails.application.config.assets.precompile += %w( comments/comment.js )
