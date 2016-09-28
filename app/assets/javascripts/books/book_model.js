6997b55bc44f68e95113fc1976a09d56d18d6e70
evanscloud <dijkstrascloud@gmail.com>
2016-09-28 15:26:36 -0400

    Render book json data into javascript object

---
+++ app/assets/javascripts/books/book_model.js
@@ -0,0 +1,14 @@
+var Book = function(id, title, author, publisher, published_date, description, price, isbn, buy_link, image_link, collections, genres){
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.publisher = publisher;
+  this.published_date = published_date;
+  this.description = description;
+  this.price = price;
+  this.isbn = isbn;
+  this.buy_link = buy_link;
+  this.image_link = image_link;
+  this.collections = collections;
+  this.genres = genres;
+};
