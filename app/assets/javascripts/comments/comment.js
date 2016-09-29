fc131c0d9bc1d969f8cc882aca9f6613783b8610
evanscloud <dijkstrascloud@gmail.com>
2016-09-29 09:13:05 -0400

    Update jquery selector to only work on new comment form

--- app/assets/javascripts/comments/comment.js
+++ app/assets/javascripts/comments/comment.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('form').submit(function(event){
+  $('#new_comment').submit(function(event){
     event.preventDefault();
 
     var values = $(this).serialize();
@@ -11,7 +11,9 @@ $(function() {
 
       commentText += '<p>' + comment.content + '</p>';
       commentText += 'Posted by: ' + comment.user.username;
-      // commentText += ;
+      // if (comment.user.id == userId){
+      //   commentText += ;
+      // }
       commentText += '<br><br>';
       $('#user-comments').append(commentText);
     });
