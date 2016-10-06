c3002e6f7b1ad710cf5936b7ef49a30a8d6244ba
evanscloud <dijkstrascloud@gmail.com>
2016-10-06 11:58:17 -0400

    Refactor user id data

--- app/assets/javascripts/comments/comment.js
+++ app/assets/javascripts/comments/comment.js
@@ -2,13 +2,16 @@ $(function() {
   $('#new_comment').submit(function(event){
     event.preventDefault();
 
+    var collectionId = $('.custcontainer').data('collection');
+    var userId = $('.form-input').data('user');
+
     var values = $(this).serialize();
     var posting = $.post('/collections/' + collectionId + '/comments', values);
 
     posting.done(function(data){
       var comment = data;
       var commentText = '';
-
+      
       commentText += '<p>' + comment.content + '</p>';
       commentText += 'Posted by: ' + comment.user.username + " ";
       if (comment.user.id == userId){
